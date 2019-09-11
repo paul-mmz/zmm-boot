5f52c1a288655b59c0512401a796249c934a0fe5
hzzhouminmin <hzzhouminmin@corp.neteasec.om>
2019-09-11 10:39:09 +0800

    commit

--- src/main/java/com/paul/zmm_boot/DemoApplication.java
+++ src/main/java/com/paul/zmm_boot/DemoApplication.java
@@ -1,16 +1,12 @@
 package com.paul.zmm_boot;
 
-import com.paul.service.ServiceOne;
-import com.paul.utils.ServiceTypeEnum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@SpringBootApplication
-@ComponentScan("com.paul")
+@SpringBootApplication(scanBasePackages = {"com.paul"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
